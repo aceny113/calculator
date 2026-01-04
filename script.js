100065b058cd484f6ce0e72bf127ad9d5062c557
aceny113 <jasonyance13@gmail.com>
2026-01-04 20:19:38 +0800

    made operate function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const add = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-const multiply = (a,b) => a * b;
-const divide = (a,b) => a / b;
+
+function operate(first, second, operator) {
+    if (operator === "add") return first + second;
+    if (operator === "subtract") return first - second;
+    if (operator === "multiply") return first * second;
+    if (operator === "divide") return first / second;
+}
