8758f86b0420a90bef0095c9014bf08d9b5a1b81
aceny113 <jasonyance13@gmail.com>
2026-01-06 04:00:29 +0800

    button clicks display inputs

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
+const numList = document.querySelectorAll(".number");
+const display = document.querySelector(".display-input");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector(".equal");
+
+let first = "";
+let second = "";
+let op = "";
+const resetDisplay = false;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
 
 function operate(first, second, operator) {
-    if (operator === "add") return first + second;
-    if (operator === "subtract") return first - second;
-    if (operator === "multiply") return first * second;
-    if (operator === "divide") return first / second;
+  if (operator === "+") return add(first, second);
+  if (operator === "-") return subtract(first, second);
+  if (operator === "*") return multiply(first, second);
+  if (operator === "/") return divide(first, second);
 }
+
+function numInput() {
+  for (let i = 0; i < numList.length; i++) {
+    numList[i].addEventListener("click", () => {
+      display.textContent += numList[i].textContent;
+    });
+  }
+
+}
+
+function operator() {
+  for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener("click", () => {
+      op = operatorButtons[i].textContent;
+      display.textContent = op;
+    });
+
+  }
+
+
+  
+}
+
+numInput();
+operator();
+operate();
+
+    if (op) {
+    resetDisplay = true;
+    display.textContent = "";
+    
+  }
